f6762f9e22c89cc64fa53aa2afede3ce5d43b136
Dmytro Lynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-02-10 17:08:28 +0200

    Delete src/components directory

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++
@@ -1,9 +0,0 @@
-import css from './LoadMoreBtn.module.css';
-
-export const LoadMoreBtn = ({ children, onClick }) => {
-  return (
-    <button className={css.loadMoreBtn} onClick={onClick}>
-      {children}
-    </button>
-  );
-};
